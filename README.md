5017addfe9e4b546530c2dd839ddcf6c3784743a
Amagioshi <rocczalez@gmail.com>
2025-09-25 22:58:13 -0300

    print para verificar commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Este repositorio tiene como propósito realizar la segunda entrega sumativa de programación orientada al trabajo.
 # este comentario  tiene como objetivo ver si el commit se puede hacer
 
+#se agrego un "hola mundo" para confirmar el commit
--- test_conexion.py
+++ test_conexion.py
@@ -1,14 +1,1 @@
-import pyodbc
-
-try:
-    conn = pyodbc.connect(
-        'DRIVER={ODBC Driver 18 for SQL Server};'
-        'SERVER=localhost\\SQLEXPRESS;'
-        'DATABASE=InventarioBodega;'
-        'Trusted_Connection=yes;'
-        'Encrypt=no;'
-    )
-    print(" Conexión exitosa a la base de datos.")
-    conn.close()
-except Exception as e:
-    print("❌ Error al conectar:", e)
+print("hola, mundo")
